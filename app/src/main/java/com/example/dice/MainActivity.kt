bde8a147302856d35d0be31801e56eaf10640099
Richo Ha <richard.dqh@gmail.com>
2022-06-23 13:32:59 +0800

    Updated so println shows on console

--- app/src/main/java/com/example/dice/MainActivity.kt
+++ app/src/main/java/com/example/dice/MainActivity.kt
@@ -7,16 +7,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
-    fun main() {
         val myFirstDice = Dice()
         println(myFirstDice.sides)
         myFirstDice.roll()
     }
 
-    class Dice{
+    class Dice {
         var sides = 6
+
         fun roll() {
             val randomNumber = (1..6).random()
             println(randomNumber)
