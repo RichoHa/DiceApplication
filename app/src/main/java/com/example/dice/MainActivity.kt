e5dbd8b616d13d7b5933de58b7dbae117f3cc7e2
Richo Ha <richard.dqh@gmail.com>
2022-06-28 21:57:54 +0800

    Added colour attribute to Dice

--- app/src/main/java/com/example/dice/MainActivity.kt
+++ app/src/main/java/com/example/dice/MainActivity.kt
@@ -11,13 +11,18 @@ class MainActivity : AppCompatActivity() {
         val myFirstDice = Dice(6)
         println("Your ${myFirstDice.numSides} sided dice rolled ${myFirstDice.roll()}!")
 
+        //Extra Task
         val mySecondDice = Dice(20)
         println("Your ${mySecondDice.numSides} sided dice rolled ${mySecondDice.roll()}!")
     }
 
     class Dice(val numSides: Int) {
+
         fun roll(): Int {
             return (1..numSides).random()
         }
     }
 }
+//TODO Give your Dice class another attribute of color and create multiple instances of dice with different numbers of sides and colors!
+//to include an additional attribute, include "val colour: String" in the Dice attributes and include the additional colour when creating an instance of Dice.
+//e.g. "val mySecondDice = Dice(20, "White")" note all instances will have to include an additional attribute.
