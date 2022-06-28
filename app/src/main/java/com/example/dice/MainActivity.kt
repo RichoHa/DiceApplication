2a0621a0f6f02612f95d6ebb3e08d47cc7a4a963
Richo Ha <richard.dqh@gmail.com>
2022-06-28 21:59:54 +0800

    added comment on head or tails game

--- app/src/main/java/com/example/dice/MainActivity.kt
+++ app/src/main/java/com/example/dice/MainActivity.kt
@@ -26,3 +26,6 @@ class MainActivity : AppCompatActivity() {
 //TODO Give your Dice class another attribute of color and create multiple instances of dice with different numbers of sides and colors!
 //to include an additional attribute, include "val colour: String" in the Dice attributes and include the additional colour when creating an instance of Dice.
 //e.g. "val mySecondDice = Dice(20, "White")" note all instances will have to include an additional attribute.
+
+//TODO Create a Coin class, give it the ability to flip, create an instance of the class and flip some coins! How would you use the random() function with a range to accomplish the coin flip?
+//reduce the range to two and depending on 1 or 2, create an if function to return heads or tail instead.
