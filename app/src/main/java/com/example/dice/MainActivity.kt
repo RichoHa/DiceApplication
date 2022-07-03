7225189f71627f12e8eb5e87956fef04afe97c0e
Richo Ha <richard.dqh@gmail.com>
2022-07-03 18:42:52 +0800

    Updated to course code, same shit

--- app/src/main/java/com/example/dice/MainActivity.kt
+++ app/src/main/java/com/example/dice/MainActivity.kt
@@ -11,45 +11,47 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        fun rollDice() {
-            val dice = Dice(6)
-            val diceRoll = dice.roll()
-            val diceImage: ImageView = findViewById(R.id.imageView)
+        val rollButton: Button = findViewById(R.id.roll)
+        rollButton.setOnClickListener { rollDice() }
 
-            when (diceRoll) {
-                1 -> diceImage.setImageResource(R.drawable.dice_1)
-                2 -> diceImage.setImageResource(R.drawable.dice_2)
-                3 -> diceImage.setImageResource(R.drawable.dice_3)
-                4 -> diceImage.setImageResource(R.drawable.dice_4)
-                5 -> diceImage.setImageResource(R.drawable.dice_5)
-                6 -> diceImage.setImageResource(R.drawable.dice_6)
-            }
-
-        }
-
-        val btnRoll = findViewById<Button>(R.id.roll)
-
-            btnRoll.setOnClickListener {
-                rollDice()
-            }
-
-//        when (rollResult) {
-//            luckyNumber -> println("You won!")
-//            1 -> println("So sorry! You rolled a 1. Try again!")
-//            2 -> println("Sadly, you rolled a 2. Try again!")
-//            3 -> println("Unfortunately, you rolled a 3. Try again!")
-//            5 -> println("Don't cry! You rolled a 5. Try again!")
-//            6 -> println("Apologies! You rolled a 6. Try again!")
-//        }
+        // Do a dice roll when the app starts
+        rollDice()
 
     }
 
-    class Dice(val numSides: Int) {
+class Dice(val numSides: Int) {
 
-        fun roll(): Int {
-            return (1..numSides).random()
-        }
+    fun roll(): Int {
+        return (1..numSides).random()
     }
+}
+
+/**
+ * Roll the dice and update the screen with the result.
+ */
+private fun rollDice() {
+    // Create new Dice object with 6 sides and roll the dice
+    val dice = MainActivity.Dice(6)
+    val diceRoll = dice.roll()
+
+    // Find the ImageView in the layout
+    val diceImage: ImageView = findViewById(R.id.imageView)
+
+    // Determine which drawable resource ID to use based on the dice roll
+    val drawableResource = when (diceRoll) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
+
+    // Update the ImageView with the correct drawable resource ID
+    diceImage.setImageResource(drawableResource)
+
+    // Update the content description
+    diceImage.contentDescription = diceRoll.toString()
 }
 
 
